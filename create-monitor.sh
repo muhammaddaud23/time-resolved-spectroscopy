47de114dfb774e3094d8a7e16dae7bd591af6d19
muhammaddaud23 <muhammaddaud23@students.itb.ac.id>
2021-06-30 11:23:59 +0700

    First Commit

---
+++ create-monitor.sh
@@ -0,0 +1,4 @@
+while IFS= read -r line; do
+	seextrct infile=$line gtiorfile=APPLY gtiandfile=gti-${line} outroot=monitor extenpha='.pha' extenlc='.lc' bitfile=bitfile-otomasi timecol=TIME columns='Event' multiple='no' binsz=0.25 mfracexp=INDEF tnull=0.0 printmode=LIGHTCURVE lcmode=SUM spmode=SUM mlcinten=INDEF mspinten=INDEF timemin=INDEF timemax=INDEF timeint=INDEF gticols='START STOP' chmin=INDEF chmax=INDEF chint=INDEF chbin=INDEF ephem=INDEF period=INDEF phaseint=INDEF obsdate='MJDREF' obstime='TSTART TSTOP' sensecase=no chkit=no clobber=yes negative=IGNORE bailout=no mode='h'
+done < "list_event"
+echo "Monitor lightcurve has been created for {line} file"
---
+++ create_timing.py
@@ -0,0 +1,58 @@
+from astropy.io import fits
+import numpy as np
+
+list_timetrans_preburst = open('list_timetrans_preburst','w')
+list_timetrans_burst = open('list_timetrans_burst','w')
+
+with open("list_burst") as list_burst:
+	for burst in list_burst:
+		burst = burst.replace("\n","")
+		hdul = fits.open(burst)
+		head = hdul[1].header
+		tstart = head['TSTART']
+		timezero = head['TIMEZERO']
+		absolute = timezero+tstart
+
+hdul = fits.open("monitor.lc")
+data = hdul[1].data
+count = data['COUNTS']
+time = data['TIME']
+
+i_t0 = np.where(time==absolute)
+i_t0 = i_t0[0][0]
+fixed_i_t0 = i_t0
+t0 = time[i_t0]
+t1s=[]
+t2s=[]
+
+while i_t0 < fixed_i_t0+60:
+	total_count = []
+	total_count.append(count[i_t0])
+	i = i_t0
+	i_t1 = i_t0
+	while sum(total_count)<1500:
+		total_count.append(count[i])
+		i=i+1
+		i_t2 = i
+		t1 = time[i_t1]
+		t2 = time[i_t2]
+		i_t0 = i_t2
+	t1s.append(t1)
+	t2s.append(t2)
+
+i=1
+for t1,t2 in zip(t1s,t2s):
+	timetrans_burst = open(f'timetrans-burst-{i}','w')
+	timetrans_burst.write(f'{t1} {t2}')
+	timetrans_burst.close()
+	list_timetrans_burst.write(f'timetrans-burst-{i}\n')
+	i = i+1
+timetrans_preburst = open('timetrans-preburst','w')
+timetrans_preburst.write('250 390') #Write background timing here
+timetrans_preburst.close()
+list_timetrans_preburst.write('timetrans-preburst')
+
+list_timetrans_burst.close()
+list_timetrans_preburst.close()
+    		
+    		
