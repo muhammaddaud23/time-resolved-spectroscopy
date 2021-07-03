2b2f44be8635428c69507313ccafef99d0220923
muhammaddaud23 <muhammaddaud23@students.itb.ac.id>
2021-07-03 23:31:07 +0700

    Revision

--- create_timing.py
+++ create_timing.py
@@ -27,14 +27,15 @@ t2s=[]
 
 while i_t0 < fixed_i_t0+60:
 	total_count = []
-	total_count.append(count[i_t0])
+	#total_count.append(count[i_t0])
 	i = i_t0
 	i_t1 = i_t0
+	t1 = time[i_t1]
 	while sum(total_count)<1500:
 		total_count.append(count[i])
 		i=i+1
 		i_t2 = i
-		t1 = time[i_t1]
+		#t1 = time[i_t1]
 		t2 = time[i_t2]
 		i_t0 = i_t2
 	t1s.append(t1)
@@ -48,11 +49,20 @@ for t1,t2 in zip(t1s,t2s):
 	list_timetrans_burst.write(f'timetrans-burst-{i}\n')
 	i = i+1
 timetrans_preburst = open('timetrans-preburst','w')
-timetrans_preburst.write('245157264.87843037 245157280.87843037') #Write background timing here
+timetrans_preburst.write('245312710.12843037 245312726.12843037') #Write background timing here
 timetrans_preburst.close()
 list_timetrans_preburst.write('timetrans-preburst')
 
 list_timetrans_burst.close()
 list_timetrans_preburst.close()
+
+##(optional) this part will create center timestamp for each data
+timestamp = open('clocktick','w')
+for t1,t2 in zip(t1s,t2s):
+	increment = (t2-t1)/2
+	delta = t1 - t1s[0]
+	stamp = increment + delta
+	timestamp.write(f'{stamp}\n')
+timestamp.close()
     		
     		
