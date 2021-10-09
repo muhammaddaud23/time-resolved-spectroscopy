f91b7d444429ff81f91e32ae3755fcdf6142d977
muhammaddaud23 <muhammaddaud23@students.itb.ac.id>
2021-10-09 23:44:48 +0700

    first commit

---
+++ run_stage_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+import numpy as np
+
+try:
+	
+
+try:
+	os.mkdir("stage-1")
+	print("===============================================")
+	print("Creating stage-1 directory to store output file")
+	print("===============================================")
+except:
+	print("=================================")
+	print("stage-1 directory already exist")
+	print("New directory will not be created")
+	print("=================================")
+	
